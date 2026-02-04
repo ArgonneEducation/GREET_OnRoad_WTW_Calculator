a06937bb7a711ec8832bf233764a585dddcae476
Michael Kollasch <mrkollasch@gmail.com>
2026-02-04 09:29:04 -0600

    Format emissions unit with subscript
    Add subscript formatting for emissions unit display

--- static/app1.js
+++ static/app1.js
@@ -1,6 +1,13 @@
 // makePanel loads the dataset, drills down to data, filters the data to get emissions data
 // and then creates a panel with the emissions data.
 
+// address subscripts
+const unit - data.unit; 
+const formattedUnit = unit.replace(/CO2e/g, 'CO<sub>2</sub>e');
+
+// Use innerHTML to insert subscripts
+document.getElementById(/unit-display').innerHTML = formattedUnit;
+
 function makePanel(fuel, metric) {
     d3.json("https://raw.githubusercontent.com/ArgonneEducation/GREET_OnRoad_WTW_Calculator/refs/heads/main/data/emissions.json").then(function (data) {
         let dataArray = data.emissions;
